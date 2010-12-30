31e9120c43dda969b670650892db481f80d36c4b
Mattijs Hoitink <mattijs@monkeyandmachine.com>
2010-12-30 14:38:05 +0100

    Added HTTP status codes to the Response class

--- lib/beep/Response.php
+++ lib/beep/Response.php
@@ -15,10 +15,68 @@ namespace beep;
 class Response
 {
     /**
+     * HTTP response status codes with their message
+     */
+    public $statuses = array (
+        // Informational 1xx
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        // Successful 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        // Redirection 3xx
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Not Used',
+        307 => 'Temporary Redirect',
+        // Client Error 4xx
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        // Server Error 5xx
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported'
+    );
+    
+    /**
+     * The HTTP procol to use
+     * @var string
+     */
+    public $protocol = 'HTTP/1.1';
+    
+    /**
      * HTTP status code
      * @var int
      */
-    public $code = 200;
+    public $status = 200;
     
     /**
      * HTTP response headers
@@ -34,10 +92,46 @@ class Response
     
     /** **/
     
+    /**
+     * Construct a new Response
+     */
     public function __construct()
     {
         // Add at least one default header
         $this->setHeader('X-Powered-By', 'Beep ' . beep::VERSION);
+    }
+    
+    /**
+     * Get and/or set the Response status code. If a new code is passed in this 
+     * will be set as the status code for the Response.
+     * @param int|NULL $status  The new status code (Optional)
+     * @return int              The status code for the response
+     */
+    public function status($status = null)
+    {
+        if (null !== $status) {
+            $this->status = $status;
+        }
+        
+        return $this->status;
+    }
+    
+    /**
+     * Returns the message for a status
+     * @param int $status   The status code to retreive the message for
+     * @return string       The message for the status code
+     */
+    public function statusMessage($status = null)
+    {
+        if (null === $status) {
+            $status = $this->status;
+        }
+        
+        if (!array_key_exists($status, $this->statuses)) {
+            throw new \Exception("Status code '{$status}' does not exist");
+        }
+        
+        return $this->statuses[$status];
     }
     
     /**
@@ -89,6 +183,11 @@ class Response
     {
         // Merge additional headers
         $this->setHeaders($headers);
+        
+        // Send the HTTP response code
+        $status = $this->status();
+        $message = $this->statusMessage();
+        header("{$this->protocol} {$status} {$message}");
         
         // Construct and send headers
         foreach ($this->headers as $header => $value) {
